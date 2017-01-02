fdb3a0e0d98ae543ec9eb3f15f68e97e746cc692
Kevin Pheasey <kevin@kpheasey.com>
2017-01-02 16:49:07 -0500

    Cast category_term as string to ensure it is correctly passed.

--- lib/cognitivebing.rb
+++ lib/cognitivebing.rb
@@ -4,27 +4,27 @@ require 'open-uri'
 require 'net/http'
 
 class CognitiveBing
-  attr_accessor :account_key, :params 
-  
+  attr_accessor :account_key, :params
+
   def initialize(account_key, params = {})
-    @account_key = account_key    
+    @account_key = account_key
     @params = params
   end
-  
-  
+
+
   def search(search_term, type = 'web')
-    
-    
+
+
     query_string = '?q='
     query_portion = URI.encode_www_form_component('\'' + search_term + '\'')
     params = "&Ocp-Apim-Subscription-Key=#{@account_key}"
     @params.each do |k,v|
       params << "&#{k.to_s}=#{v.to_s}"
-      
+
     end
-    
+
     web_search_url = ""
-    
+
     if type == "videos"
       web_search_url = "https://api.cognitive.microsoft.com/bing/v5.0/videos/search"
     elsif type == "image"
@@ -32,48 +32,48 @@ class CognitiveBing
     else
       web_search_url = "https://api.cognitive.microsoft.com/bing/v5.0/search"
     end
-    
+
     full_address = web_search_url + query_string + query_portion + params
-    
+
     uri = URI(full_address)
     req = Net::HTTP::Get.new(uri.request_uri)
     req.add_field("Ocp-Apim-Subscription-Key", @account_key)
-    
+
 
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https'){|http|
       http.request(req)
     }
 
     body = JSON.parse(res.body, :symbolize_names => true)
-        
-    
+
+
     return body
   end
-  
+
   def suggestions(search_term)
-    
-    
+
+
     query_string = '?q='
     query_portion = URI.encode_www_form_component( search_term )
-    
-    
-    
+
+
+
     web_search_url = "https://api.cognitive.microsoft.com/bing/v5.0/suggestions"
-    
-    full_address = web_search_url + query_string + query_portion 
-    
+
+    full_address = web_search_url + query_string + query_portion
+
     uri = URI(full_address)
     req = Net::HTTP::Get.new(uri.request_uri)
     req.add_field("Ocp-Apim-Subscription-Key", @account_key)
-    
+
 
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https'){|http|
       http.request(req)
     }
 
     body = JSON.parse(res.body, :symbolize_names => true)
-        
-    
+
+
     return body
   end
 end
@@ -81,89 +81,89 @@ end
 
 class CognitiveBingNews
   attr_accessor :account_key
-  
+
   def initialize(account_key, params = {})
     @account_key = account_key
     @params = params
   end
-  
+
   def search(search_term)
-    
-    
+
+
     query_string = '?q='
     query_portion = URI.encode_www_form_component('\'' + search_term + '\'')
     paramsbuilder = "&Ocp-Apim-Subscription-Key=#{@account_key}"
     @params.each do |k,v|
       paramsbuilder << "&#{k.to_s}=#{v.to_s}"
-      
-      
+
+
     end
-    
+
     web_search_url = "https://api.cognitive.microsoft.com/bing/v5.0/news/search"
-    
-    
-    full_address = web_search_url + query_string + query_portion + paramsbuilder   
-    
-    
+
+
+    full_address = web_search_url + query_string + query_portion + paramsbuilder
+
+
     uri = URI(full_address)
     req = Net::HTTP::Get.new(uri.request_uri)
     req.add_field("Ocp-Apim-Subscription-Key", @account_key)
-    
+
 
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https'){|http|
       http.request(req)
     }
 
     body = JSON.parse(res.body, :symbolize_names => true)
-        
-    
+
+
     return body
   end
-  
-  
-  
+
+
+
   def category(category_term, params = {})
-      
+
     web_search_url = URI('https://api.cognitive.microsoft.com/bing/v5.0/news/')
       web_search_url.query = URI.encode_www_form({
           # Request parameters
-          'category' => '#{category_term}'
+          'category' => category_term.to_s
     })
-    
-    
-    
+
+
+
     uri = URI(web_search_url)
     req = Net::HTTP::Get.new(uri.request_uri)
     req.add_field("Ocp-Apim-Subscription-Key", @account_key)
-    
+
 
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https'){|http|
       http.request(req)
     }
 
     body = JSON.parse(res.body, :symbolize_names => true)
-        
-    
+
+
     return body
   end
-  
-  
+
+
   def trending
-    
+
     web_search_url = "https://api.cognitive.microsoft.com/bing/v5.0/news/trendingtopics"
-    
-    
+
+
     uri = URI(web_search_url)
     req = Net::HTTP::Get.new(uri.request_uri)
     req.add_field("Ocp-Apim-Subscription-Key", @account_key)
-    
+
 
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https'){|http|
       http.request(req)
     }
 
     body = JSON.parse(res.body, :symbolize_names => true)
-    
+
     return body
   end
 end
